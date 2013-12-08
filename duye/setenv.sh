e5453d100858f84e186bb891d9bd750d7609186a
tencupofkaiwater <wangwei2135@gmail.com>
2013-12-08 07:45:11 -0800

    modify

--- duye/setenv.sh
+++ duye/setenv.sh
@@ -1,4 +1,4 @@
-export DUYE_ROOT=/root/share/github_svn/duye/trunk/
+export DUYE_ROOT=/root/share/github_svn/duye/trunk
 export DUYE_LIB=$DUYE_ROOT/duye/lib
 export DUYE_BIN=$DUYE_ROOT/duye/bin
 mkdir $DUYE_LIB -p
