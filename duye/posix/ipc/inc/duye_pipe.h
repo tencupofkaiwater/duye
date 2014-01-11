2ef35ea9b9b6d5f7e3b1bd7450887477be209519
tencupofkaiwater <wangwei2135@gmail.com>
2014-01-11 14:44:38 +0800

    Update duye_pipe.h
    add fifo code

--- duye/posix/ipc/inc/duye_pipe.h
+++ duye/posix/ipc/inc/duye_pipe.h
@@ -21,11 +21,65 @@
 
 DUYE_POSIX_NS_BEG 
 
+// brief : 进程通信，管道基类，被读管道和写管道继承
+// 	
 class Pipe
 {
 public:
-	Pipe();
-	~Pipe();
+	Pipe() {}
+	virtual ~Pipe() {}
+	
+	// brief : 打开管道
+	// @para pipeName 管道名
+	// return true/false
+	// note
+	D_BOOL Open(const stl::string& pipeName) = 0;
+	
+proteced:
+	// 管道描述符
+	D_INT32		m_pipefd;
 };
+
+// brief : 写管道
+// usage :
+//	void MyWritePipe()
+//	{
+// 		WritePipe writePipe;
+//		if (writePipe.Open("/tmp/testFifoFile"))
+//		{
+//		
+//		}
+//	}
+class WritePipe : public Pipe
+{
+public:
+	WritePipe() {}
+	virtual ~WritePipe() {}
+	
+	// brief : 打开管道
+	// @para pipeName 管道名
+	// return true/false
+	// note
+	D_BOOL Open(const stl::string& pipeName);	
+	
+	// brief : 向管道写数据
+	// @para data 写入数据
+	// @para dataLen 写入数据长度
+	// return true/false
+	// note
+	D_BOOL Write(D_INT8* data, const D_UINT32 dataLen);
+	
+private:
+	// brief : 防止拷贝
+	WritePipe(const WritePipe&);
+	void operator=(const WritePipe&);
+};
+
+// brief : 读管道
+// usage :
+//	
+
+
+
 
 DUYE_POSIX_NS_END 
