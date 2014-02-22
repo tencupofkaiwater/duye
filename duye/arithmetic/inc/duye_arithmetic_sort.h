20d314dfa6951e07357ace882654a83a016b8f4a
tencupofkaiwater <wangwei2135@gmail.com>
2014-02-21 18:45:42 -0800

    add files

---
+++ duye/arithmetic/inc/duye_arithmetic_sort.h
@@ -0,0 +1,30 @@
+/************************************************************************************
+**  
+*    @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		duye_arithmetic_sort.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-02-22
+* @note 
+*
+*  1. 2014-02-22 duye Created this file
+* 
+*/
+
+#pragma once
+
+#include <duye/arithmetic/inc/duye_arithmetic_def.h>
+
+DUYE_ARITHMETIC_NS_BEG
+
+// brief : arithmetic wrapper class
+class Sort
+{
+    static void BubbleSort();
+};
+
+DUYE_ARITHMETIC_NS_END
---
+++ duye/arithmetic/src/duye_arithmetic_sort.cpp
@@ -0,0 +1,27 @@
+/************************************************************************************
+**  
+*    @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		duye_arithmetic_sort.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-02-22
+* @note 
+*
+*  1. 2014-02-22 duye Created this file
+* 
+*/
+
+#include <duye/arithmetic/inc/duye_arithmetic_sort.h>
+
+DUYE_ARITHMETIC_NS_BEG
+
+void Sort::BubbleSort()
+{
+    
+}
+
+DUYE_ARITHMETIC_NS_END
